379572b98b47750bd5caf523af29c7ba96ee2b56
Forever1ove <2651935952@qq.com>
2021-06-14 11:34:23 -0700

    11

--- Tower Defense Unity Project/Assets/Scripts/CompleteLevel.cs
+++ Tower Defense Unity Project/Assets/Scripts/CompleteLevel.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class CompleteLevel : MonoBehaviour {
 
 	public string menuSceneName = "MainMenu";
 
+    public int gameLevel;
 	public string nextLevel = "Level02";
-	public int levelToUnlock = 2;
+    public string nextLevel2 = "Level03";
+    public string nextLevel3 = "Level04";
+    public int levelToUnlock = 4;
 
 	public SceneFader sceneFader;
 
@@ -15,7 +21,15 @@ public class CompleteLevel : MonoBehaviour {
 	{
 		PlayerPrefs.SetInt("levelReached", levelToUnlock);
 		sceneFader.FadeTo(nextLevel);
-	}
+
+        
+
+            
+        //SceneManager.LoadScene(gameLevel);
+        
+
+
+    }
 
 	public void Menu ()
 	{
